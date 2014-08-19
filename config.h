1d9683fc855a7b53a01f8152ac9db9dfc56c7a1d
Jeffrey Tolar <tolar.jeffrey@gmail.com>
2014-08-19 15:03:43 -0500

    Change font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Meslo LG S DZ for Powerline:pixelsize=12,Inconsolata\\ for\\ Powerline:pixelsize=12";
+static char font[] = "Meslo LG S DZ:pixelsize=12,Inconsolata:pixelsize=12;PowerlineSymbols:pixelsize=12";
 static int borderpx = 0;
 static char shell[] = "/usr/bin/zsh";
 
