d970872dbdd2cc2e48e29cc28371f82d5073a61f
Jeffrey Tolar <tolar.jeffrey@gmail.com>
2014-09-06 16:24:20 -0500

    Increase initial font size

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Meslo LG S DZ for Powerline:pixelsize=12";
+static char font[] = "Meslo LG S DZ for Powerline:size=14";
 static int borderpx = 0;
 static char shell[] = "/usr/bin/zsh";
 
