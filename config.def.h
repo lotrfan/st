25b07f86a51c37114f7b27979483dbf129e0df93
Jeffrey Tolar <tolar.jeffrey@gmail.com>
2015-10-10 16:06:29 -0500

    Revert "Add plumbing"
    This reverts commit 420a777d11130ebe6f174687c4d696956ba2c3c7.

--- config.def.h
+++ config.def.h
@@ -128,8 +128,6 @@ static Shortcut shortcuts[] = {
 	{ MODKEY|ShiftMask,     XK_C,           clipcopy,       {.i =  0} },
 	{ MODKEY|ShiftMask,     XK_V,           clippaste,      {.i =  0} },
 	{ MODKEY,               XK_Num_Lock,    numlock,        {.i =  0} },
-	{ MODKEY,               XK_u,           plumb,          {.v = "urlselect"} },
-	{ MODKEY|ShiftMask,     XK_U,           plumb,          {.v = "urlselect -c"} },
 };
 
 /*
